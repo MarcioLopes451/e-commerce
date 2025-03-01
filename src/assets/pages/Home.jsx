88fa45a1f4fc1ec51698ad176839524fa34dcf14
Jason <jsonwu34@gmail.com>
2025-02-28 20:22:37 -0500

    add: navbar

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Ad from "../components/ad/Ad"
+import Navbar from "../components/navbar/Navbar"
 
 function Home() {
   return (
     <div className="w-screen">
         <Ad />
+            <Navbar />
     </div>
   )
 }
