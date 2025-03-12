5c14ef1d521ea6dd56a5cbf9daaa2781c2503b70
Marcio Lopes <79576331+MarcioLopes451@users.noreply.github.com>
2025-03-12 18:28:04 +0000

    add: more edits...

--- src/assets/layout/EcommerceLayout.jsx
+++ src/assets/layout/EcommerceLayout.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Ad from '../components/ad/Ad';
-import Navbar from '../components/navbar/Navbar';
-import { Outlet } from 'react-router';
+import React from "react";
+import Ad from "../../components/ad/Ad";
+import Navbar from "../../components/navbar/Navbar";
+import { Outlet } from "react-router";
 
 export const EcommerceLayout = () => {
   return (
